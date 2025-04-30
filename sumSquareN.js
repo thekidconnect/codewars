55d1d00d0b48f93e3e4296181dccab1ba191a931
Chibu Ichiban <the1kidconnect@gmail.com>
2025-04-30 16:11:31 -0700

    added second squareSum function

--- sumSquareN.js
+++ sumSquareN.js
@@ -2,5 +2,9 @@ function squareSum(numbers){
     return numbers.map(n=>n**2).reduce((a,b)=>a+b,0)
   }
 
-  const testResults = squareSum([11,8,-12,11,7,-19,16,-1,-12])
+  function squareSumTwo(numbers){
+    return numbers.reduce((sum,num)=>sum+num**2,0)
+  }
+
+  const testResults = squareSumTwo([11,8,-12,11,7,-19,16,-1,-12])
   console.log(testResults)

3aca0244ee3cc11374da66a1b3dc62634659a7f9
Chibu Ichiban <the1kidconnect@gmail.com>
2025-06-03 12:11:18 -0700

    implemented boolean to string conversion function

---
+++ 8Kyu/booleanToString.js
@@ -0,0 +1,9 @@
+function booleanToStringOne(b){
+    //your code here
+    return b ? "true" : "false"
+  }
+
+const booleanToStringTwo = b => `${b}`
+
+const testResults=booleanToStringTwo(false)
+console.log(testResults)

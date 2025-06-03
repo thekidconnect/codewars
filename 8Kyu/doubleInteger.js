7405b2884ec67914d2d2285c248ecea6430c2a79
Chibu Ichiban <the1kidconnect@gmail.com>
2025-06-03 12:04:05 -0700

    implemented integer-doubling function

---
+++ 8Kyu/doubleInteger.js
@@ -0,0 +1,9 @@
+function doubleIntegerOne(i) {
+    // i will be an integer. Double it and return it.
+    return 2*i;
+  }
+
+const doubleIntegerTwo = (i) => i*2
+
+const testResults=doubleIntegerTwo(2)
+console.log(testResults)
